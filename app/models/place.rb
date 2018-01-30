94d9b09f20ae3dce0ef1e3485c076466b92639ee
Jaques Dias <jaquesdias19@gmail.com>
2018-01-30 12:40:33 -0200

    Small adjustments on import method

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,15 +3,15 @@ class Place < ApplicationRecord
     file_path = "#{Rails.public_path}/filialstamm.csv"
     raise "File does not exist" unless File.exists? file_path
 
-    CSV.foreach(file_path, headers: true, col_sep: ";", encoding:'iso-8859-1:utf-8') do |row|
+    CSV.foreach(file_path, headers: true, header_converters: :symbol, col_sep: ";", encoding:'iso-8859-1:utf-8') do |row|
       Place.create!(
-        customer_id:  row["Kundennummer"],
-        store_number: row["Filialnummer"],
-        store_name:   row["Filialname"],
-        street:       row["Strasse"],
-        zip_code:     row["PLZ"],
-        place:        row["ORT"]
-      )
+        customer_id:  row[:kundennummer].to_i,
+        store_number: row[:filialnummer].to_i,
+        store_name:   row[:filialname].to_s.strip,
+        street:       row[:strasse].to_s.strip,
+        zip_code:     row[:plz].to_s.strip,
+        place:        row[:ort].to_s.strip
+      ) unless row.empty?
     end
   end
 end
